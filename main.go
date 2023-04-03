73194d7a43083658b0322740ef79662a62cd9081
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2023-04-03 14:55:35 +0100

    Created the main server

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+
 func main() {
+	http.HandleFunc("/", handleWebSocket)
+	log.Println("Server started at localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		// read message from client
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		// print the message to the console
+		fmt.Printf("Received message: %s\n", msg)
+
+		// send a message back to the client
+		if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+			log.Println(err)
+			break
+		}
+	}
+}
